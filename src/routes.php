7724b4d3575af6960b0ff06b6488ff34fe78ffb6
Adam Herstein <aherstein@users.noreply.github.com>
2018-02-05 14:50:06 -0800

    Added HTTPS to all routes

--- src/routes.php
+++ src/routes.php
@@ -8,31 +8,37 @@ Route::group([
 
     Route::get('/logout', array(
         'as' => 'saml_logout',
+        'https' => 'true', 
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@logout',
     ));
 
     Route::get('/login', array(
         'as' => 'login',
+        'https' => 'true', 
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@login',
     ));
 
     Route::get('/sso', array(
         'as' => 'saml_sso_form',
+        'https' => 'true', 
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@samlCapture',
     ));
 
     Route::get('/metadata', array(
         'as' => 'saml_metadata',
+        'https' => 'true', 
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@metadata',
     ));
 
     Route::post('/acs', array(
         'as' => 'saml_acs',
+        'https' => 'true', 
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@acs',
     ));
 
     Route::get('/sls', array(
         'as' => 'saml_sls',
+        'https' => 'true', 
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@sls',
     ));
 });
